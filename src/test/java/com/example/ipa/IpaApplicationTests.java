75663f14b810e30605a4386cc6f0a56c5b3dc86a
Krzysztof Morikis <k.morikis@tim.pl>
2020-04-05 11:24:53 +0200

    initial commit

---
+++ src/test/java/com/example/ipa/IpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
